c9396ec25fa9790e41af381b26822295edc7fdbb
aymen <35670@ma-web.nl>
2022-11-24 10:20:16 +0100

    update

--- les 1/app.js
+++ les 1/app.js
@@ -3,23 +3,16 @@ class App
     runApplication()
     {
         console.log("hello world!");
-        console.log(appNaam)
-        console.log(versienummer)
-        console.log(versiedatum)
-        console.log(autheur)
-        console.log(copyright)
-        console.log(distributeur)
-        console.log(darkmode)
+        let appNaam = "AymensApp"
+        let versienummer = "0.9 double"
+        let versiedatum = new Date("2022-11-25");
+        let autheur = "Aymen"
+        let copyright = "Aymen"
+        let distributeur = "Aymen"
+        let darkmode = Boolean(10 > 9)
     }
 }
 
 let app = new App();
 app.runApplication();
 
-let appNaam = "AymensApp"
-let versienummer = "0.9 double"
-let versiedatum = new Date("2022-11-25");
-let autheur = "Aymen"
-let copyright = "Aymen"
-let distributeur = "Aymen"
-let darkmode = Boolean(10 > 9)
