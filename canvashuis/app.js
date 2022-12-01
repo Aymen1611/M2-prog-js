680c8b674ae6b2021902a44d2578c32d572c9c80
aymen <35670@ma-web.nl>
2022-12-01 12:25:09 +0100

    canvas update

--- canvashuis/app.js
+++ canvashuis/app.js
@@ -1,24 +1,82 @@
 class App
 {
     runApplication()
-    {   
-        
+    {
         console.log("hello world!");
-        let canvas = document.getElementById("canvasid")
+        let canvas = document.getElementById("canvasId")
+
+        console.log(canvas)
+
         let g = canvas.getContext("2d");
-         
+       
+        g.fillRect(0,0,10,10);
+
+       
+
+
+        
+       
         g.beginPath();
-        g.moveTo(300, 300);
-        g.lineTo(25, 25, 100, 100);
-        g.lineTo(150, 40, 125, 200);
-        g.lineTo(25, 150, 100, 125);
+        g.strokeStyle = "gray";
+        g.fillStyle = "lightgray";
+        g.moveTo(200, 300);
+        g.lineTo(200, 500);
+        g.lineTo(600, 675);
+        g.lineTo(600, 400);
         g.closePath();
         g.stroke();
-        g.fill()
+        g.fill();
 
-    }   
+        g.beginPath();
+        g.strokeStyle = "gray";
+        g.fillStyle = "brown"
+        g.moveTo(600, 675);
+        g.lineTo(815, 555);
+        g.lineTo(815, 350);
+        g.lineTo(600, 450)
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+        
+
+       
+
+
+        g.beginPath();
+        g.strokeStyle = "brown";
+        g.fillStyle = "brown "
+        g.moveTo(815, 350);
+        g.lineTo(700, 200);
+        g.lineTo(600, 450)
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+        
+        g.beginPath();
+        g.strokeStyle = "brown";
+        g.fillStyle = "green";
+        g.moveTo(300, 100);
+        g.lineTo(700, 200);
+        g.lineTo(600, 450);
+        g.lineTo(200, 300);
+        g.closePath();
+        g.stroke();
+        g.fill();
+
+        g.beginPath();
+        g.fillStyle = "lightblue";
+        g.strokeStyle = "gray";
+        g.moveTo(265, 350);
+        g.lineTo(265, 450);
+        g.lineTo(363, 490);
+        g.lineTo(365, 385);
+        g.lineTo(265, 350);
+        g.fill();
+        
+    }
 }
 
 let app = new App();
 app.runApplication();
-
